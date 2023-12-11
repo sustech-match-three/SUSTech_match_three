122e0d19612289704e9102a3a98ac0ece1e74bca
NiZenMeZhiDao <12313401@mail.sustech.edu.cn>
2023-12-11 22:25:42 +0800

    niznemezhidao

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ import javax.swing.*;
 
 public class Main {
     // test
+
+
+
+    // tttt
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             ChessGameFrame mainFrame = new ChessGameFrame(1100, 810);
