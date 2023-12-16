7bdb8d03f12254f9a6710dbc5a0466dc401b0f4b
NiZenMeZhiDao <12313401@mail.sustech.edu.cn>
2023-12-17 00:04:54 +0800

    加了个bgm

--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -1,33 +1,34 @@
-//import javax.sound.sampled.*;
-//import java.io.File;
-//import java.io.IOException;
-//public class MyRunnable implements Runnable{
-//
-//    @Override public void run() {
-//        try {
-//            MusicPlayer.playMusic();
-//        }catch (Exception e){
-//            System.out.println(e);
-//        }
-//
-//    } public class MusicPlayer{
-//        public static final String bgm="";
-//        static Clip music=null;
-//        static File file=null;
-//        public static void playMusic(String path){
-//            try {
-//                music= AudioSystem.getClip();
-//                file=new File(path);
-//                AudioInputStream audioInputStream=AudioSystem.getAudioInputStream(file);
-//                music.open(audioInputStream);
-//                music.start();
-//            }catch (LineUnavailableException e){
-//                e.printStackTrace();
-//            }catch (UnsupportedAudioFileException e){
-//                e.printStackTrace();
-//            }catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//}
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class MyRunnable implements Runnable{
+
+    @Override public void run() {
+        try {
+            MusicPlayer.playMusic("MyScoure/et9s0-eb3ho.wav");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    } public class MusicPlayer{
+        public static final String bgm="MyScoure/et9s0-eb3ho.wav";
+        static Clip music=null;
+        static File file=null;
+        public static void playMusic(String path){
+            try {
+                music= AudioSystem.getClip();
+                file=new File(path);
+                AudioInputStream audioInputStream=AudioSystem.getAudioInputStream(file);
+                music.open(audioInputStream);
+                music.loop(-1);
+                music.start();
+            }catch (LineUnavailableException e){
+                e.printStackTrace();
+            }catch (UnsupportedAudioFileException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
